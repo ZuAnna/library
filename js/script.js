69d4ff075969d0e0b145fa634526ef02354bff5b
ZuAnna <eva.anna.ru@gmail.com>
2022-03-15 15:38:59 +0300

    add block nobooks and stylized

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 
 let allbooks = document.querySelector('.allbooks');
 let childAllbooks = allbooks.childNodes;
-console.log(childAllbooks);
+function createNobooks(){
+   let nobooks = document.createElement('div');
+   nobooks.classList.add('allbooks__nobooks');
+   allbooks.appendChild(nobooks);
+   let paragraph = document.createElement('p');
+   paragraph.classList.add('allbooks__nobooks-text');
+   nobooks.appendChild(paragraph);
+   paragraph.innerText = 'Ваша библиотека пуста'
+}
 let nobooks = document.createElement('div');
 if (childAllbooks.length <= 1) {
-   nobooks.classList.add('allbooks__noboks');
-   allbooks.appendChild(nobooks);
+   createNobooks();
+   // nobooks.classList.add('allbooks__noboks');
+   // allbooks.appendChild(nobooks);
 } else if (childAllbooks.length > 1){
    allbooks.removeChild(nobooks);
 }
-console.log(allbooks);
